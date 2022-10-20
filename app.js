9a14d3bbab2eb1ce23adc1daeb11587d2d98b32e
Apurv Shingne <110399600+ApurvShingne@users.noreply.github.com>
2022-10-20 14:55:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello searce!'))
+app.get('/', (req, res) => res.send('Hello everyone!'))
 app.listen(3000, () => console.log('Server ready'))
